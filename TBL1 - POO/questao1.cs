44bd3f91ec18051f78a87bde186643e0d0282fa4
Angelo Zanatta <angeloantoquirinozanatta@gmail.com>
2026-02-25 22:49:22 -0300

    TBL1 de POO

---
+++ TBL1 - POO/questao1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = a;
+
+        b = 20;
+
+        Console.WriteLine("Valor de a: " + a);
+        Console.WriteLine("Valor de b: " + b);
+    }
+}
