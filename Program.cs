042d1a9e2fc75f4f1f0bef457cbea422a150415e
Angelo Zanatta <angeloantoquirinozanatta@gmail.com>
2026-04-08 15:44:02 -0300

    organizacao

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Faculdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
